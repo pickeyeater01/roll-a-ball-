f5f44accf27e3476feabc28e6edcfb449420c6d0
cristian tapia <cristiantapia@cristians-MacBook-Pro-2.local>
2019-02-04 22:11:26 -0800

    finished
    final product

---
+++ Assets/scripts/player_controller.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_controller : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
+    public Text space;
+    public float jumpHeight;
+    public bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (isGrounded)
+        {
+           
+        }
+
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void Update()
+    {
+            if (Input.GetButtonDown("Jump")
+            && isGrounded)
+        {
+            Vector3 movement2 = new Vector3(0, 7, 0);
+        rb.AddForce(movement2 * jumpHeight);
+            }
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("pick up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+
+        countText.text = "Count:" + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!!!";
+        }
+    }
+}
